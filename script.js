e580ebddeb6552e5f01777f3350cad7ea5ebd8a3
Reichardt-G <guilhermeneves.reichardt@gmail.com>
2024-11-30 13:07:12 -0300

    feat: riscar tarefas completas

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let BotaoAdd  = document.getElementById('botao');
 let Input = document.getElementById('input-tarefa');
 let Tarefas = document.getElementById('tarefas');
+let marcado = false;
 
 BotaoAdd.addEventListener('click', function() {
     if (Input.value != '') {
@@ -8,5 +9,17 @@ BotaoAdd.addEventListener('click', function() {
         Tarefas.appendChild(tarefa);
         tarefa.innerText = `- ${Input.value}`;
         Input.value = '';
+    }
+});
+
+Tarefas.addEventListener('click', function(evento) {
+    if (evento.target.tagName === 'P'){
+        if (marcado == false) {
+            evento.target.style.textDecoration = 'line-through';
+            marcado = true;
+        } else {
+            evento.target.style.textDecoration = '';
+            marcado = false;
+        }
     }
 });
